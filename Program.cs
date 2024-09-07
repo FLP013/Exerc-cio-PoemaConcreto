03e1253f6d42b0083cdd8735dd6dbc8b31bae0bd
FLP013 <felliperibeiro9@gmail.com>
2024-09-07 00:33:00 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+ 
+Console.WriteLine(@"
+                    u m
+                      m o v i  
+                      m e n t o
+                    compondo
+                   além
+                          da
+                  nuvem
+                     um
+                    campo
+                       de
+                    combate
+                   
+                      mira
+                    gem
+                       ira
+                          de
+                     um
+                       horizonte
+puro
+   num
+     mo
+     mento
+ vivo");
